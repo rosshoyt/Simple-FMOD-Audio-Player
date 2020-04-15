7eaa32f98d6447e051a7fa17bac5e62b4859621d
unknown <rosshoytmusic@gmail.com>
2020-04-14 19:10:43 -0700

    Update demo audiofile, documentation

--- Simple-FMOD-Audio-Player.cpp
+++ Simple-FMOD-Audio-Player.cpp
@@ -5,30 +5,35 @@
 #include <fmod.hpp>
 #include <Windows.h>
 
-FMOD::System* sys; // FMOD's low-level audio system
+FMOD::System* sys;                           // FMOD's low-level audio system
+const char* AUDIO_FILE = "mx_section_1.ogg"; // 00:31:30 loopable music file
 
-const char* AUDIO_FILE = "mx_section_1.ogg"; // 30 second loopable music file
-int nSeconds = 60;                           // how long to play sound
-
-void playSoundFMOD(const char* filename, bool loop = false) {
+void playSoundFMOD(const char* filename, bool loop = false, int nLoops = 1) {
     // sound    
     FMOD::Sound* sound;
     // the sound's channel
     FMOD::Channel* channel;
     // create sound
     sys->createSound(filename, loop ? FMOD_LOOP_NORMAL : FMOD_LOOP_OFF, 0, &sound);
-    // assign channel and play sound
+    
+    // get length of sound file (to allow sound playback by sleeping main thread for this amt of time)
+    unsigned int msLen; 
+    sound->getLength(&msLen, FMOD_TIMEUNIT_MS);
+    //msLen *= nLoops; // adjust playback length by number of requested loops
+
+    // assign channel and begin to play sound
     sys->playSound(sound, 0, true /*start paused*/, &channel);
-    // actually play sound
+    // actually start the sound
     channel->setPaused(false);
+
+    // let song play by sleeping main thread for some time
+    Sleep(msLen * nLoops);
 }
 
 int main() {
     FMOD::System_Create(&sys);
     sys->init(32 /*max number of audio channels*/, FMOD_INIT_NORMAL, 0);
-    playSoundFMOD(AUDIO_FILE, true);
-    // let song play by sleeping main thread for some time
-    Sleep(nSeconds * 1000);
+    playSoundFMOD(AUDIO_FILE, true, 1);
     return 0;
 }
 
